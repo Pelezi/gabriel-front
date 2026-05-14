748d42f2402999cda435485ba59847b84763dc96
Alessandro <54072990+Pelezi@users.noreply.github.com>
2026-05-14 14:40:16 -0300

    docker dev

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,16 @@ const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   output: 'standalone',
+  turbopack: {},
+  webpack: (config, { dev }) => {
+    if (dev) {
+      config.watchOptions = {
+        poll: 1000,
+        aggregateTimeout: 2000,
+      };
+    }
+    return config;
+  },
   images: {
     remotePatterns: [
       {
